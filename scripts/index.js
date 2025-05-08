953f07f71dc2aa102884ea809d0e928c2f20273a
Maxim Smirnov <seymj@mail.ru>
2025-05-08 18:31:56 +0300

    5-ый спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+// Получение шаблона, контейнера и массива карточек
+const cardTemplate = document.querySelector("#card-template").content;
+const cardsContainer = document.querySelector(".places__list");
+let cards = initialCards;
 
-// @todo: DOM узлы
+// Функция удаления карточки
+function deleteCard(element) {
+  element.remove();
+}
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, delHandler) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => delHandler(cardElement));
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция рендеринга карточек
+function renderCards(createHandler, delHandler) {
+  cards.forEach((cardData) => {
+    const renderedCard = createHandler(cardData, delHandler);
+    cardsContainer.append(renderedCard);
+  });
+}
+
+// Вызов рендеринга карточек
+renderCards(createCard, deleteCard);
