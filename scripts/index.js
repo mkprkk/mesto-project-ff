35fbaf13e54683207ec6df436fc4b7c0d51ea781
Maxim Smirnov <seymj@mail.ru>
2025-05-08 18:24:38 +0300

    Render and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+const placeList = document.querySelector(".places__list");
+
+function deleteCard(element) {
+  const deleteButton = element.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => element.remove());
+}
+
+function renderCards(cards, delHandler) {
+  cards.forEach((cardData) => {
+    const cardElement = cardTemplate
+      .querySelector(".places__item")
+      .cloneNode(true);
+      
+    cardElement.querySelector(".card__image").src = cardData.link;
+    cardElement.querySelector(".card__title").textContent = cardData.name;
+
+    delHandler(cardElement);
+
+    placeList.append(cardElement);
+  });
+}
+
+renderCards(initialCards, deleteCard);
